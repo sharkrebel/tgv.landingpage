7dd38c587ad4fd2c7bde046f6293d7fa2c1424df
sharkrebel <ntruongson@gmail.com>
2026-02-07 13:54:10 +0700

    Final 1.0.5a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,11 +60,6 @@ export const metadata: Metadata = {
       "max-snippet": -1,
     },
   },
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
-  },
 };
 
 export default function RootLayout({
